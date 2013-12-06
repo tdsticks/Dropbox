98c47989c9b2c0c956a13068a317b1639469969e
Steve Stickel <tdsticks@gmail.com>
2013-12-06 10:57:02 -0500

    Clean cred py file (you'll need to update this with your own DB / API info

---
+++ dev/Credentials.py
@@ -0,0 +1,10 @@
+
+class Credentials:
+
+	def __init__(self):
+
+		self.KEY 		= 'Enter your API Key here'
+		self.SECRET 	= 'Enter your API Secret here'
+
+		self.usr 		= 'Enter your DB email (username)'
+		self.pwd 		= 'Enter your DB password'
